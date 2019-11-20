d3ddad9d534bc2d06b453394f8ff0f5dfff47cf8
Wouter van der Wal <33957974+wjtje@users.noreply.github.com>
2019-11-20 21:14:16 +0100

    added main

--- index.js
+++ index.js
@@ -6,10 +6,35 @@ $('header nav div.activeTip').css({
   width: $('header nav a')[0].offsetWidth + 2
 });
 
-// Animate activeTib when tab changes
+// Animate activeTib when tab changes and change main
 $('header nav a').click(e => {
   $('header nav div.activeTip').css({
     left: e.target.offsetLeft - 1,
     width: e.target.offsetWidth + 2
   });
+
+  $('main').removeClass('active').addClass('nonactive');
+  $('*[data-name="' + e.target.dataset.name + '"]').addClass('active').removeClass('nonactive');
+
+  /*$('main').css({
+    left: -$(window)[0].innerWidth,
+    'z-index': 1
+  });
+
+  $('*[data-name="' + e.target.dataset.name + '"]').css({
+    left: 0
+  });*/
 });
+
+// Set main pages to correct place
+/*$('main').css({
+  left: -$(window)[0].innerWidth,
+  'z-index': 1
+});
+
+$(window).resize(e => {
+  $('main').css({
+    left: -$(window)[0].innerWidth,
+    'z-index': 1
+  });
+});*/
