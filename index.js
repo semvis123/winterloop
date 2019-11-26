2baf4c71a460a1c06d9becfd1cdfc5b5211c4d3e
Wouter van der Wal <33957974+wjtje@users.noreply.github.com>
2019-11-26 17:37:32 +0100

    New hover effect

--- index.js
+++ index.js
@@ -1,40 +1,40 @@
 import $ from 'jquery';
 
+function updateActiveTip(left, width, object) {
+  // Update normal vars
+  $('header nav div.activeTip').get(0).style.setProperty("--left", left + 5 + 'px');
+  $('header nav div.activeTip').get(0).style.setProperty("--width", width - 10 + 'px');
+
+  // Update hover vars
+  $('header nav div.activeTip').get(0).style.setProperty("--hoverLeft", left + 'px');
+  $('header nav div.activeTip').get(0).style.setProperty("--hoverWidth", width + 'px');
+
+  // Update hover event
+  $('header nav a').off('mouseenter mouseleave');
+  $(object).on('mouseenter', e => {
+    $('div.activeTip').addClass('hover')
+  }).on('mouseleave', e => {
+    $('div.activeTip').removeClass('hover')
+  });
+}
+
 // Setup activeTip in ribbon
-$('header nav div.activeTip').css({
-  left: $('header nav a')[0].offsetLeft - 1,
-  width: $('header nav a')[0].offsetWidth + 2
-});
+updateActiveTip(
+  $('header nav a')[0].offsetLeft,
+  $('header nav a')[0].offsetWidth,
+  $('header nav a')[0]
+);
+
+// Load correct page
+$('main').removeClass('active').addClass('nonactive');
+$('main[data-name="' + $('header nav a')[0].dataset.name + '"]').addClass('active');
 
 // Animate activeTib when tab changes and change main
 $('header nav a').click(e => {
-  $('header nav div.activeTip').css({
-    left: e.target.offsetLeft - 1,
-    width: e.target.offsetWidth + 2
-  });
+  // Update css vars
+  updateActiveTip(e.target.offsetLeft, e.target.offsetWidth, $('header nav a[data-name="' + e.target.dataset.name + '"]'));
 
+  // Update main
   $('main').removeClass('active').addClass('nonactive');
-  $('*[data-name="' + e.target.dataset.name + '"]').addClass('active').removeClass('nonactive');
-
-  /*$('main').css({
-    left: -$(window)[0].innerWidth,
-    'z-index': 1
-  });
-
-  $('*[data-name="' + e.target.dataset.name + '"]').css({
-    left: 0
-  });*/
+  $('main[data-name="' + e.target.dataset.name + '"]').addClass('active').removeClass('nonactive');
 });
-
-// Set main pages to correct place
-/*$('main').css({
-  left: -$(window)[0].innerWidth,
-  'z-index': 1
-});
-
-$(window).resize(e => {
-  $('main').css({
-    left: -$(window)[0].innerWidth,
-    'z-index': 1
-  });
-});*/
