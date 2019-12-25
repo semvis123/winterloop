ef483095df42e22651ac05102f38d3b22bcf22c5
Wouter van der Wal <33957974+wjtje@users.noreply.github.com>
2019-12-25 15:01:02 +0100

    Header and menu working

--- src/header.tsx
+++ src/header.tsx
@@ -1,11 +1,25 @@
 import React = require('react');
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+
+// Header
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+
+// Drawer
+import Drawer from '@material-ui/core/Drawer';
+import Divider from '@material-ui/core/Divider';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+
+// Icons
+import CreateIcon from '@material-ui/icons/Create';
+import PollIcon from '@material-ui/icons/Poll';
+import AccountBalanceWalletIcon from '@material-ui/icons/AccountBalanceWallet';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -18,17 +32,44 @@ const useStyles = makeStyles((theme: Theme) =>
     title: {
       flexGrow: 1,
     },
+    list: {
+      width: 250,
+    },
+    drawerTitle: {
+      marginLeft: 16,
+      marginTop: 110
+    },
+    drawerSubTitle: {
+      marginLeft: 16,
+      marginBottom: 11
+    }
   }),
 );
 
 export default function ButtonAppBar(props) {
+  // Vars
   const classes = useStyles(props);
+  const [state, setState] = React.useState({
+    left: false
+  });
 
+  // Functions
+  const toggleDrawer = (open: boolean) => (
+    event: React.KeyboardEvent | React.MouseEvent
+  ) => {
+    if (event.type === "keydown" && ((event as React.KeyboardEvent).key === 'Tab' || (event as React.KeyboardEvent).key === 'Shift')) {
+      return;
+    }
+
+    setState({ ...state, ['left']: open });
+  }
+
+  // Render
   return (
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu" onClick={toggleDrawer(true)}>
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" className={classes.title}>
@@ -36,6 +77,32 @@ export default function ButtonAppBar(props) {
           </Typography>
         </Toolbar>
       </AppBar>
+      <Drawer open={state.left} onClose={toggleDrawer(false)}>
+        <div
+          className={classes.list}
+          role="presentation"
+          onClick={toggleDrawer(false)}
+          onKeyDown={toggleDrawer(false)}
+        >
+          <List>
+            <Typography variant="h6" className={classes.drawerTitle}>Winterloop</Typography>
+            <Typography variant="body2" className={classes.drawerSubTitle}>Administratie</Typography>
+            <Divider />
+            <ListItem button key="Registratie">
+              <ListItemIcon><CreateIcon /></ListItemIcon>
+              <ListItemText primary="Registratie"/>
+            </ListItem>
+            <ListItem button key="Stempels">
+              <ListItemIcon><PollIcon /></ListItemIcon>
+              <ListItemText primary="Stempels"/>
+            </ListItem>
+            <ListItem button key="Transacties">
+              <ListItemIcon><AccountBalanceWalletIcon /></ListItemIcon>
+              <ListItemText primary="Transacties"/>
+            </ListItem>
+          </List>
+        </div>
+      </Drawer>
     </div>
   );
 }
