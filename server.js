a64c25427f08033d1710c2f2b3c50339b471965d
semvis123 <semvisscher10@gmail.com>
2019-12-27 20:39:06 +0100

    .

--- server.js
+++ server.js
@@ -4,25 +4,25 @@ var http = require('http');
 console.log('SERVER: starting');
 
 var con = mysql.createConnection({
-  host: "localhost",
-  user: "demo",
-  password: "password"
+    host: "localhost",
+    user: "nodejs",
+    password: "SQL123"
 });
 
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("SERVER: connected to mysql");
+con.connect(function (err) {
+    if (err) throw err;
+    console.log("SERVER: connected to mysql");
 });
 
 console.log("SERVER: created");
 http.createServer((req, res) => { // Request, Response
-  console.log('SERVER: response');
-  con.query('SELECT naam FROM winterloop.user', (e, r, f) => { // Error, Result, Field
-    console.log(r);
-    res.write('Response from server: ');
-    r.forEach(ele => {
-      res.write(ele.naam + ' ');
+    console.log('SERVER: response');
+    con.query('SELECT naam FROM winterloop.user', (e, r, f) => { // Error, Result, Field
+        console.log(r);
+        res.write('Response from server: ');
+        r.forEach(ele => {
+            res.write(ele.naam + ' ');
+        });
+        res.end();
     });
-    res.end();
-  });
-}).listen(8080);
+}).listen(4322);
