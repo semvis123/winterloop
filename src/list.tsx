98f4a1f122f8c10b85d28253eeedf4e70c99c66f
semvis123 <semvisscher10@gmail.com>
2019-12-29 20:30:52 +0100

    Update list.tsx

--- src/list.tsx
+++ src/list.tsx
@@ -1,5 +1,5 @@
 import React = require('react');
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import { createStyles, makeStyles, Theme, withStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
@@ -12,15 +12,54 @@ import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import Button from '@material-ui/core/Button';
 import { fullScreenDialog } from './index';
-const useStyles = makeStyles((theme: Theme) =>
+import BedumerTheme from './theme';
+
+const style = makeStyles((theme: Theme) =>
   createStyles({
     root: {
+      flexGrow: 1,
+      color: (localStorage.getItem('dark') == 'true') ? 'rgba(255, 255, 255, 0.87)' : 'rgba(0, 0, 0, 0.87)',
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+    list: {
+      width: 250,
+    },
+    drawerTitle: {
+      marginLeft: 16,
+      marginTop: 10
+    },
+    drawerSubTitle: {
+      marginLeft: 16,
+      marginBottom: 11
+    },
+    logo: {
+      margin: 16,
+      width: 218,
+      boxSizing: 'content-box'
+    },
+    fab: {
+      position: 'absolute',
+      bottom: 16,
+      right: 26,
+    },
+    settingsList: {
       width: '100%',
+      maxWidth: 360,
       backgroundColor: theme.palette.background.paper,
     },
+    text: {
+      color: (localStorage.getItem('dark') == 'true') ? 'rgba(255, 255, 255, 0.87)' : 'rgba(0, 0, 0, 0.87)',
+    }
   }),
 );
-export default class PersonList extends React.Component {
+
+
+class PersonList extends React.Component {
 
   constructor(props) {
     super(props);
@@ -30,7 +69,6 @@ export default class PersonList extends React.Component {
       dialog: false,
       currentPerson: null
     };
-
 
   }
   getData(){
@@ -44,14 +82,13 @@ export default class PersonList extends React.Component {
   }
 
   render() {
+    const { classes } = this.props;
     const { persons } = this.state;
-
-
     return (
       <div>
-           {this.state.persons? (<List>
+           {this.state.persons? (<List className={classes.root}>
              {persons.map((person,i) =>
-               <ListItem button key={i} onClick={e=>this.setState({dialog:true, currentPerson: persons[i]})}>
+               <ListItem divider button key={i} onClick={e=>this.setState({dialog:true, currentPerson: persons[i]})}>
                  <ListItemText id={person.id} primary={person.naam} secondary={person.code} />
                </ListItem>
 
@@ -75,3 +112,4 @@ export default class PersonList extends React.Component {
 
   }
 }
+export default withStyles(style)(PersonList);
