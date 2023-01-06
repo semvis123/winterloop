74231ca5bbd5f09c00586db343b763f8e7478fcf
semvis123 <semvisscher10@gmail.com>
2023-01-06 15:02:17 +0100

    Update server.js

--- server.mjs
+++ server.mjs
@@ -1,12 +1,19 @@
-const mysql = require('mysql');
-const express = require('express');
-const util = require('util');
-const cors = require('cors');
-const app = express().use('*', cors());
-const bodyParser = require('body-parser');
+import { createRequire } from 'node:module';
+const require = createRequire(import.meta.url);
+
 const Config = require('./configuration.json');
+
+import { faker } from '@faker-js/faker/locale/nl';
+import mysql from 'mysql';
+import express from 'express';
+import util from 'util';
+import cors from 'cors';
+
+import bodyParser from 'body-parser';
+
+const app = express().use('*', cors());
 const port = Config.server.port;
-var faker = require('faker/locale/nl');
+
 
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({
@@ -14,7 +21,7 @@ app.use(bodyParser.urlencoded({
 })); // support encoded bodies
 app.disable("x-powered-by");
 
-var con = mysql.createConnection(Config.mysql);
+const con = mysql.createConnection(Config.mysql);
 
 console.log('SERVER: starting');
 
@@ -50,7 +57,7 @@ app.get('/code/', async (req, res) => {
 app.get('/api/getUsers/', (req, res) => {
     con.query('SELECT * FROM winterloop.user ORDER BY naam', (e, r) => { // Error, Result
         console.log(r);
-        var arr = new Array();
+        const arr = new Array();
         r.forEach(ele => {
             arr.push({
                 'id': ele.id,
@@ -70,8 +77,8 @@ app.get('/api/getUsers/', (req, res) => {
     });
 })
 app.post('/api/addUser/', async (req, res) => {
-    var code = await genCode();
-    var par = req.body; // get parameters from url
+    const code = await genCode();
+    const par = req.body; // get parameters from url
     con.query("INSERT INTO winterloop.user (`naam`,`huisnummer`,`postcode`,`telefoonnummer`,`vastBedrag`,`rondeBedrag`,`code`)" +
       " VALUES (?,?,?,?,?,?,?)", [par.naam, par.huisnummer, par.postcode, par.telefoonnummer, par.vastBedrag, par.rondeBedrag, code], (e) => {
         if (e) {
@@ -82,7 +89,7 @@ app.post('/api/addUser/', async (req, res) => {
     });
 });
 app.post('/api/editUser/', async (req, res) => {
-    var par = req.body; // get parameters from url
+    const par = req.body; // get parameters from url
     con.query("UPDATE winterloop.user SET naam = ?, huisnummer = ?,postcode = ?,telefoonnummer = ?,vastBedrag = ?, rondeBedrag = ? WHERE id=?",
      [par.naam, par.huisnummer, par.postcode, par.telefoonnummer, par.vastBedrag, par.rondeBedrag, par.id], (e) => {
         if (e) {
@@ -93,7 +100,7 @@ app.post('/api/editUser/', async (req, res) => {
     });
 });
 app.post('/api/addRound/', async (req, res) => {
-    var par = req.body; // get parameters from url
+    const par = req.body; // get parameters from url
     con.query("UPDATE winterloop.user SET rondes = rondes + 1 WHERE code = ?", [par.code], (e) => {
         if (e) {
             res.status(500).send(e.sqlMessage);
@@ -103,7 +110,7 @@ app.post('/api/addRound/', async (req, res) => {
     });
 });
 app.post('/api/setPayed/', async (req, res) => {
-    var par = req.body; // get parameters from url
+    const par = req.body; // get parameters from url
     con.query("UPDATE winterloop.user SET betaald = ? WHERE code = ?", [par.payed, par.code], (e) => {
         if (e) {
             res.status(500).send(e.sqlMessage);
@@ -114,7 +121,7 @@ app.post('/api/setPayed/', async (req, res) => {
 });
 
 app.post('/api/removeRound/', async (req, res) => {
-    var par = req.body; // get parameters from url
+    const par = req.body; // get parameters from url
     con.query("UPDATE winterloop.user SET rondes = rondes - 1 WHERE code = ?", [par.code], (e) => {
         if (e) {
             res.status(500).send(e.sqlMessage);
@@ -124,7 +131,7 @@ app.post('/api/removeRound/', async (req, res) => {
     });
 });
 app.post('/api/setRound/', async (req, res) => {
-    var par = req.body; // get parameters from url
+    const par = req.body; // get parameters from url
     con.query("UPDATE winterloop.user SET rondes = ? WHERE code = ?", [par.rondes, par.code], (e) => {
         if (e) {
             res.status(500).send(e.sqlMessage);
@@ -135,7 +142,7 @@ app.post('/api/setRound/', async (req, res) => {
 });
 app.post('/api/removeUser/', async (req, res) => {
     console.log(req);
-    var par = req.body;
+    const par = req.body;
     con.query("DELETE FROM winterloop.user WHERE code = ?", [par.code], e => {
         if (e) {
             res.status(500).send(e.sqlMessage);
@@ -146,7 +153,6 @@ app.post('/api/removeUser/', async (req, res) => {
 });
 app.post('/api/emptyDB/', async (req, res) => {
     console.log(req);
-    var par = req.body;
     con.query("DELETE FROM winterloop.user", e => {
         if (e) {
             res.status(500).send(e.sqlMessage);
@@ -158,11 +164,11 @@ app.post('/api/emptyDB/', async (req, res) => {
 
 app.post('/api/fillDB/', async (req, res) => {
     // randomly fills database
-    var par = req.body;
+    const par = req.body;
     console.log(req);
-    for (i = 0; i < par.count; i++) {
-        var code = await genCode();
-        pers = faker.helpers.userCard(); // generates random user
+    for (let i = 0; i < par.count; i++) {
+        const code = await genCode();
+        const pers = faker.helpers.userCard(); // generates random user
         console.log(pers)
         con.query("INSERT INTO winterloop.user (`naam`,`huisnummer`,`postcode`,`telefoonnummer`,`vastBedrag`,`rondeBedrag`,`code`)" +
         " VALUES (?,?,?,?,?,?,?)", [pers.name, pers.address.suite.replace("[a-zA-Z]", ""), pers.address.zipcode.replace(" ", ""), pers.phone,(Math.random() * 1000.00).toFixed(2),(Math.random() * 1000.00).toFixed(2), code], (e) => {
